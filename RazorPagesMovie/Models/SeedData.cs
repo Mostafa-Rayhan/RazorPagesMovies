9731d77ac90d1783e04da406676f6ca8d75d0af4
Mostafa Rayhan <mostafa.rayhan@fintechhub.com.bd>
2024-07-30 12:45:09 +0600

    add a new field

--- RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/Models/SeedData.cs
@@ -1,61 +1,75 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿#define Rating
+#if Rating
+#region snippet_1 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
+using System;
+using System.Linq;
 
-namespace RazorPagesMovie.Models;
-
-public static class SeedData
+namespace RazorPagesMovie.Models
 {
-    public static void Initialize(IServiceProvider serviceProvider)
+    public static class SeedData
     {
-        using (var context = new RazorPagesMovieContext(
-            serviceProvider.GetRequiredService<
-                DbContextOptions<RazorPagesMovieContext>>()))
+        public static void Initialize(IServiceProvider serviceProvider)
         {
-            if (context == null || context.Movie == null)
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
             {
-                throw new ArgumentNullException("Null RazorPagesMovieContext");
-            }
-
-            // Look for any movies.
-            if (context.Movie.Any())
-            {
-                return;   // DB has been seeded
-            }
-
-            context.Movie.AddRange(
-                new Movie
+                if (context == null || context.Movie == null)
                 {
-                    Title = "When Harry Met Sally",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                    Genre = "Romantic Comedy",
-                    Price = 7.99M
-                },
-
-                new Movie
-                {
-                    Title = "Ghostbusters ",
-                    ReleaseDate = DateTime.Parse("1984-3-13"),
-                    Genre = "Comedy",
-                    Price = 8.99M
-                },
-
-                new Movie
-                {
-                    Title = "Ghostbusters 2",
-                    ReleaseDate = DateTime.Parse("1986-2-23"),
-                    Genre = "Comedy",
-                    Price = 9.99M
-                },
-
-                new Movie
-                {
-                    Title = "Rio Bravo",
-                    ReleaseDate = DateTime.Parse("1959-4-15"),
-                    Genre = "Western",
-                    Price = 3.99M
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
                 }
-            );
-            context.SaveChanges();
+
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                #region snippet1
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                #endregion
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "NA"
+                    }
+                );
+                context.SaveChanges();
+            }
         }
     }
 }
+#endregion
+#endif
